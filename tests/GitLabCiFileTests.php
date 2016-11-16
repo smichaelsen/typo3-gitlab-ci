89e3dbdd88aa0d5f5059d95ab4e4bdeb248341c3
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-11-16 22:53:50 +0100

    WIP: Test validity of gitlab-ci file

--- tests/GitLabCiFileTests.php
+++ tests/GitLabCiFileTests.php
@@ -14,18 +14,12 @@ class GitLabCiFileTests extends TestCase
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://appzap.githost.io/api/v3/ci/lint");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 1);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents(__DIR__ . '/../src/gitlab-ci.yml'));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        $result = curl_exec($ch);
-        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $header = substr($result, 0, $header_size);
-        $body = substr($result, $header_size);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        $result = json_decode(curl_exec($ch), true);
         curl_close($ch);
-        var_dump([$header, $body]);
-        $this->assertTrue(true);
+        $this->assertEquals('valid', $result['status']);
     }
 
 }
