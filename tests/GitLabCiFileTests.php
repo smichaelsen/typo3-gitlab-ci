64e10cf54afdf1469578afadebf56f0a420e36b7
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-11-16 22:41:00 +0100

    WIP: Test validity of gitlab-ci file

--- tests/GitLabCiFileTests.php
+++ tests/GitLabCiFileTests.php
@@ -15,7 +15,7 @@ class GitLabCiFileTests extends TestCase
         curl_setopt($ch, CURLOPT_URL, "https://gitlab.com/api/v3/ci/lint");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents(__DIR__ . '../src/gitlab-ci.yml'));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents(__DIR__ . '/../src/gitlab-ci.yml'));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         $result = curl_exec($ch);
         var_dump($result);
