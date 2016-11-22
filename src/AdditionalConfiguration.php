aea46a9c8de5160653f1f5e6e9ad20a87102ff13
Sebastian Michaelsen <sebastian@michaelsen.io>
2016-11-22 21:13:06 +0100

    [TASK] Make AdditionalConfiguration compatible with TYPO3 7 and 8

--- src/AdditionalConfiguration.php
+++ src/AdditionalConfiguration.php
@@ -2,10 +2,18 @@
 
 $environmentConfiguration = parse_ini_file(__DIR__ . '/../../files/environmentConfiguration.txt');
 
-$GLOBALS['TYPO3_CONF_VARS']['DB']['database'] = $environmentConfiguration['DBNAME'];
-$GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = $environmentConfiguration['DBPASS'];
-$GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = $environmentConfiguration['DBUSER'];
-$GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = $environmentConfiguration['DBHOST'];
+$credentials = [
+    'database' => $environmentConfiguration['DBNAME'],
+    'password' => $environmentConfiguration['DBPASS'],
+    'username' => $environmentConfiguration['DBUSER'],
+    'host' => $environmentConfiguration['DBHOST'],
+];
+
+if ((int)TYPO3_branch[0] === 7) {
+    $GLOBALS['TYPO3_CONF_VARS']['DB'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['DB'], $credentials);
+} else {
+    $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'] = array_merge($GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default'], $credentials);
+}
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = str_replace('[LIVE]', '[' . $environmentConfiguration['ENVNAME'] . ']', $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename']);
 
