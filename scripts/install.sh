655e78f0a73b1bcc26f2cc8a9e99a199f9b4b197
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-02-24 12:37:41 +0100

    [FEATURE] Custom script: pre-deploy.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,4 @@
 [ -d gitlab-ci-scripts ] || mkdir gitlab-ci-scripts
 cp vendor/smichaelsen/typo3-gitlab-ci/src/.gitlab-ci.yml.dist .gitlab-ci.yml
 cp vendor/smichaelsen/typo3-gitlab-ci/scripts/build-extensions.sh gitlab-ci-scripts/_build-extensions.sh
+cp vendor/smichaelsen/typo3-gitlab-ci/scripts/pre-deploy.sh gitlab-ci-scripts/_pre-deploy.sh
