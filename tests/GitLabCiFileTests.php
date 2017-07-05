979f169928ef6a02e22fd6d4bab79e44d82637e5
Sebastian Michaelsen <sebastian@app-zap.de>
2017-07-05 12:11:09 +0200

    [BUGFIX] Fix endpoint url to test ci file validity

--- tests/GitLabCiFileTests.php
+++ tests/GitLabCiFileTests.php
@@ -13,7 +13,7 @@ class GitLabCiFileTests extends TestCase
     public function gitlabCiFileValid()
     {
         $client = new Client([
-            'base_uri' => 'https://appzap.githost.io/api/v3/',
+            'base_uri' => 'https://maschinenraum.githost.io/api/v3/',
         ]);
         $data = json_encode([
             'content' => file_get_contents(__DIR__ . '/../src/.gitlab-ci.yml.dist'),
