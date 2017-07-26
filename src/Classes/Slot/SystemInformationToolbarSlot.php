082bab9c42c806ebc203174b5737ecf7cca8c1af
Göran Bodenschatz <coding@46halbe.de>
2017-07-26 13:46:30 +0200

    Adapt iconIdentifier definition for TYPO3 CMS 8

--- src/Classes/Slot/SystemInformationToolbarSlot.php
+++ src/Classes/Slot/SystemInformationToolbarSlot.php
@@ -2,6 +2,9 @@
 namespace Smichaelsen\Typo3GitlabCi\Slot;
 
 use TYPO3\CMS\Backend\Backend\ToolbarItems\SystemInformationToolbarItem;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SystemInformationToolbarSlot
 {
@@ -12,11 +15,18 @@ class SystemInformationToolbarSlot
     public function addItems(SystemInformationToolbarItem $systemInformationToolbarItem)
     {
         $gitReference = getenv('GIT_REFERENCE');
+        $iconIdentifier = 'sysinfo-git';
+
+        if ((int) TYPO3_branch[0] === 7) {
+            $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+            $iconIdentifier = $iconFactory->getIcon($iconIdentifier, Icon::SIZE_SMALL)->render();
+        }
+
         if (!empty($gitReference)) {
             $systemInformationToolbarItem->addSystemInformation(
                 'Git reference',
                 substr($gitReference, 0, 8),
-                '<i class="fa fa-git"></i>'
+                $iconIdentifier
             );
         }
     }
