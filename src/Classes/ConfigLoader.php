28d8524d0a9ee682d7a20a471bdf1aa5d860d213
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-10-06 20:21:52 +0200

    [TASK] Move config loading code to new class

---
+++ src/Classes/ConfigLoader.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Smichaelsen\Typo3GitlabCi;
+
+use Smichaelsen\Typo3GitlabCi\Slot\SystemInformationToolbarSlot;
+use TYPO3\CMS\Backend\Backend\ToolbarItems\SystemInformationToolbarItem;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\SignalSlot\Dispatcher;
+
+class ConfigLoader
+{
+
+    public function populate()
+    {
+        $this->loadDatabaseCredentials();
+        $this->loadAdditionalConfigurationValues();
+        $this->registerSlots();
+    }
+
+    protected function loadDatabaseCredentials()
+    {
+        if ((int) TYPO3_branch[0] === 7) {
+            $dbConfig = [
+                'database' => getenv('DBNAME'),
+                'password' => getenv('DBPASS'),
+                'username' => getenv('DBUSER'),
+                'host' => getenv('DBHOST'),
+            ];
+        } else {
+            $dbConfig = [
+                'Connections' => [
+                    'Default' => [
+                        'charset' => 'utf8',
+                        'dbname' => getenv('DBNAME'),
+                        'driver' => 'mysqli',
+                        'host' => getenv('DBHOST'),
+                        'password' => getenv('DBPASS'),
+                        'port' => '3306',
+                        'user' => getenv('DBUSER'),
+                    ],
+                ],
+            ];
+        }
+        $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
+            $GLOBALS['TYPO3_CONF_VARS'],
+            ['DB' => $dbConfig]
+        );
+    }
+
+    protected function loadAdditionalConfigurationValues()
+    {
+        if (!empty(getenv('ENCRYPTION_KEY'))) {
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['encryptionKey'] = getenv('ENCRYPTION_KEY');
+        }
+
+        if (!empty(getenv('INSTALL_TOOL_PASSWORD'))) {
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['installToolPassword'] = md5(getenv('INSTALL_TOOL_PASSWORD'));
+        }
+
+        if (!empty(getenv('IM_PATH'))) {
+            $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_path'] = getenv('IM_PATH');
+            $GLOBALS['TYPO3_CONF_VARS']['GFX']['im_path_lzw'] = getenv('IM_PATH');
+        }
+
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] = sprintf(
+            '[%s] %s',
+            getenv('ENVNAME'),
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename']
+        );
+    }
+
+    protected function registerSlots()
+    {
+        GeneralUtility::makeInstance(Dispatcher::class)->connect(
+            SystemInformationToolbarItem::class,
+            'getSystemInformation',
+            SystemInformationToolbarSlot::class,
+            'addItems'
+        );
+    }
+}
