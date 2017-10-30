cc7a676f828b24e67742d5bd45da42bb93e517a9
Sebastian Michaelsen <sebastian@michaelsen.io>
2017-10-30 21:49:35 +0100

    [BUGFIX] Copy AdditionalConfiguration.php after writing .env

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 [ -d gitlab-ci-scripts ] || mkdir gitlab-ci-scripts
-if [ ! -f web/typo3conf/AdditionalConfiguration.php ]; then
-    cp vendor/smichaelsen/typo3-gitlab-ci/src/AdditionalConfiguration.php web/typo3conf/
-fi
 touch .env
 echo $'DBNAME='"$(./vendor/bin/typo3cms configuration:showactive --json --path DB/Connections/Default/dbname)"$'' >> .env
 echo $'DBPASS='"$(./vendor/bin/typo3cms configuration:showactive --json --path DB/Connections/Default/password)"$'' >> .env
 echo $'DBUSER='"$(./vendor/bin/typo3cms configuration:showactive --json --path DB/Connections/Default/user)"$'' >> .env
 echo $'DBHOST='"$(./vendor/bin/typo3cms configuration:showactive --json --path DB/Connections/Default/host)"$'' >> .env
+if [ ! -f web/typo3conf/AdditionalConfiguration.php ]; then
+    cp vendor/smichaelsen/typo3-gitlab-ci/src/AdditionalConfiguration.php web/typo3conf/
+fi
 cp vendor/smichaelsen/typo3-gitlab-ci/src/.gitlab-ci.yml.dist .gitlab-ci.yml
 cp vendor/smichaelsen/typo3-gitlab-ci/scripts/after-composer.sh gitlab-ci-scripts/_after-composer.sh
 cp vendor/smichaelsen/typo3-gitlab-ci/scripts/build-extensions.sh gitlab-ci-scripts/_build-extensions.sh
